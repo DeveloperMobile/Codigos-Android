b3e805968cb6239e3e3ed5053ed96f7bf490cbf2
Tiago Vieira <tiagovieirasilva@gmail.com>
2017-11-14 15:20:19 -0300

    Projeto Calcula Gorjetas

---
+++ CalcularGorjetas/app/src/main/java/com/codigosandroid/calculargorjetas/MainActivity.java
@@ -0,0 +1,130 @@
+package com.codigosandroid.calculargorjetas;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private EditText edtTotalConta;
+    private RadioGroup radioGroup;
+
+    private double total=0.0;
+
+    private DecimalFormat format;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inicializar();
+
+    }
+
+    // Inicializa os componentes
+    private void inicializar() {
+
+        // Formato monetário
+        format = new DecimalFormat("#,###.##", new DecimalFormatSymbols(new Locale("pt", "BR")));
+        format.setMinimumFractionDigits(2);
+        format.setParseBigDecimal(true);
+        // EditText
+        edtTotalConta = (EditText) findViewById(R.id.edtTotalConta);
+        // RadioGroup
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        // Calcula a gorjeta
+        /* Se não houver nenhum atributo de Button a ser alterado/utilizado
+         * pode-se referenciá-lo diretamente pela id */
+        findViewById(R.id.btnCalcular).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                switch (radioGroup.getCheckedRadioButtonId()) {
+                    case R.id.rbDez:
+                        dialog("Total com acréscimo de 10%", "Total: " + format.format(mostrarTotal()) + "\n" +
+                                "10%: " + format.format(calculaPercentual(.1)) + "\n" +
+                                "Total + 10%: " + format.format(calculaTotal(10)));
+                        break;
+                    case R.id.rbQuinze:
+                        dialog("Total com acréscimo de 15%", "Total: " + format.format(mostrarTotal()) + "\n" +
+                                "15%: " + format.format(calculaPercentual(.15)) + "\n" +
+                                "Total + 15%: " + format.format(calculaTotal(15)));
+                        break;
+                    case R.id.rbVinte:
+                        dialog("Total com acréscimo de 20%", "Total: " + format.format(mostrarTotal()) + "\n" +
+                                "20%: " + format.format(calculaPercentual(.20)) + "\n" +
+                                "Total + 20%: " + format.format(calculaTotal(20)));
+                        break;
+                    default: total = 0;
+                }
+
+            }
+
+        });
+    }
+
+    private double calculaTotal(int percentual) {
+
+        double totalGeral = 0;
+
+        switch (percentual) {
+            case 10: totalGeral =  total + calculaPercentual(.1); break;
+            case 15: totalGeral = total + calculaPercentual(.15); break;
+            case 20: totalGeral = total + calculaPercentual(.20); break;
+        }
+
+        return totalGeral;
+
+    }
+
+    private double calculaPercentual(double percentual) {
+
+        // Calcula o total da conta com a gorjeta
+        return (total * percentual);
+
+    }
+
+    private double mostrarTotal() {
+
+        try {
+
+            total = Double.parseDouble(edtTotalConta.getText().toString().replace(",", "."));
+
+        } catch (NumberFormatException e) {
+
+            System.out.println("Erro: " + e.getMessage());
+            total = 0.0;
+
+        }
+
+        return total;
+
+    }
+
+    private void dialog(String titulo, String mensagem) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setPositiveButton("OK", null);
+        builder.setTitle(titulo);
+        builder.setMessage(mensagem);
+        builder.setIcon(android.R.drawable.ic_menu_info_details);
+        builder.show();
+
+    }
+
+}
